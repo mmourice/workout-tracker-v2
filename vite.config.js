839c312799e6d79b9eade02f6516cbe7d0755c02
mmourice <minamouricee@gmail.com>
2025-09-28 12:36:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: set to your REPO NAME with trailing slash.
-// Your repo is "workout-tracker-v2"
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: exactly your repo name with trailing slash
   base: "/workout-tracker-v2/",
 });
