4ae8e7b249afc9be4ce5c25e8eb9a7508cf1c775
mmourice <minamouricee@gmail.com>
2025-09-28 12:45:21 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./modern.css";
-import { StoreProvider } from "./store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StoreProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </StoreProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
