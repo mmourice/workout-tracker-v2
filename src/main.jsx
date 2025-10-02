525ce6d4a3d97ff377de7fb36bd582cdcbdee525
mmourice <minamouricee@gmail.com>
2025-10-02 17:42:46 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+import "./modern.css";
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./modern.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
